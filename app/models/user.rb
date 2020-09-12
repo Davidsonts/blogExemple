b1ed57b3ef94eefac172420aadd2cee0941cb214
Davidson <davidsonts@outlook.com>
2020-09-12 19:28:23 -0300

    trestle:auth:install User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :posts
   enum kind: [:salesman, :manager]
   enum state: [:active, :inactive]
+  include Trestle::Auth::ModelMethods
+  include Trestle::Auth::ModelMethods::Rememberable
 end
